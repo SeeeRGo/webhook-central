58c4e817af8ac4403a8473d4a6f68a1c41afd07e
Мизин Сергей Юрьевич (4091265) <mizin@vtb.ru>
2023-08-07 13:38:27 +0400

    form parsing added 2

--- src/app/api/hr-form/route.ts
+++ src/app/api/hr-form/route.ts
@@ -1,7 +1,6 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 // @ts-ignore
-import { Formidable } from "formidable";
 import { NextApiResponse } from 'next';
 
 // //set bodyparser
