57a0a7bd2e1b9812a795d9dc6b6ea0f5f1f16d22
Мизин Сергей Юрьевич (4091265) <mizin@vtb.ru>
2023-08-07 13:25:43 +0400

    form parsing added

--- src/app/api/hr-form/route.ts
+++ src/app/api/hr-form/route.ts
@@ -1,5 +1,15 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
+import { Formidable } from "formidable";
+import { NextApiResponse } from 'next';
+
+//set bodyparser
+export const config = {
+  api: {
+    bodyParser: false
+  }
+}
+
 export function GET(request: NextRequest) {
   // Add output webhook here
   console.log('request', {
@@ -22,28 +32,46 @@ export function GET(request: NextRequest) {
   );
 }
 
-export async function POST(request: NextRequest) {
+export async function POST(request: NextRequest, res: NextApiResponse) {
+  const data = await new Promise((resolve, reject) => {
+    const form = new Formidable();
+
+    // @ts-ignore
+    form.parse(request, (err, fields, files) => {
+      if (err) reject({ err })
+      console.log('fields', fields);
+      
+      resolve({ err, fields, files })
+    }) 
+  })
+
+  //return the data back or just do whatever you want with it
+  res.status(200).json({
+    status: 'ok',
+    data
+  })
+}
   // Add output webhook here
-  // @ts-ignore
-  const {Name, Email, Name_2, Input, data, $_POST} = request.body
+
+  // const {Name, Email, Name_2, Input, data, $_POST} = request.body
   // try {
   // } catch (e) {
   //   console.log('error', e);
     
   // }
-  console.log('request', {
-    Name, Email, Name_2, Input, data, $_POST
-  });
+  // console.log('request', {
+  //   Name, Email, Name_2, Input, data, $_POST
+  // });
   
-  return NextResponse.json(
-    {
-      // body,
-      path: request.nextUrl.pathname,
-      query: request.nextUrl.search,
-      cookies: request.cookies.getAll(),
-    },
-    {
-      status: 200,
-    },
-  );
+  // return NextResponse.json(
+  //   {
+  //     // body,
+  //     path: request.nextUrl.pathname,
+  //     query: request.nextUrl.search,
+  //     cookies: request.cookies.getAll(),
+  //   },
+  //   {
+  //     status: 200,
+  //   },
+  // );
 }
