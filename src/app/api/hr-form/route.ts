5864e59b32ae3ac317ed1ca7a5a2e8de85137269
Мизин Сергей Юрьевич (4091265) <mizin@vtb.ru>
2023-08-09 17:23:56 +0400

    хантфлоу попытка интеграции

--- src/app/api/hr-form/route.ts
+++ src/app/api/hr-form/route.ts
@@ -1,51 +1,61 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
-// //set bodyparser
-// export const config = {
-//   runtime: 'edge',
-//   api: {
-//     bodyParser: false
-//   }
-// }
-
-export function GET(request: NextRequest) {
-  // Add output webhook here
-  console.log('request', {
-    body: request.body,
-    path: request.nextUrl.pathname,
-    query: request.nextUrl.search,
-    cookies: request.cookies.getAll(),
-  });
-  
-  return NextResponse.json(
-    {
-      body: request.body,
-      path: request.nextUrl.pathname,
-      query: request.nextUrl.search,
-      cookies: request.cookies.getAll(),
-    },
-    {
-      status: 200,
-    },
-  );
-}
-
 export async function POST(request: NextRequest) {
   const data = await request.formData()
-  const {Name, Email, Name_2, Input} = Object.fromEntries(data)
-  const body = { name: Name, email: Email, city: Name_2, resume_link: Input}
-  console.log('body', body);
+  console.log('data', data);
   
+  const {Name, Email, Name_2, Input} = Object.fromEntries(data)
+  const body = { first_name: Name.toString().split(' ')[0], last_name: Name.toString().split(' ')[1] ?? '' , email: Email, city: Name_2, resume_link: Input}
+
+  const hunfFlowBody = {
+    "first_name": body.first_name,
+    "last_name": body.last_name,
+    "money": "",
+    "phone": "",
+    "email": body.email,
+    "skype": "",
+    "position": "",
+    "company": "",
+    "birthday": "",
+    "externals": [
+      {
+        "auth_type": "NATIVE",
+        "data": {
+          "body": body.resume_link
+        },
+      }
+    ],
+  }
+
+  await fetch("https://api.huntflow.ru/v2/accounts/170849/applicants", {
+    "headers": {
+      "authorization": "Bearer 814d6054241c75581dca8a3fed0317b66a79349fadba3b49be42f250d883354d",
+      "content-type": "application/json",
+    },
+    "body": JSON.stringify(hunfFlowBody),
+    "method": "POST"
+  });
   return NextResponse.json(
     {
-      body,
-      path: request.nextUrl.pathname,
-      query: request.nextUrl.search,
-      cookies: request.cookies.getAll(),
+      result: 'OK',
     },
     {
       status: 200,
     },
   );
 }
+
+// {
+//   "items": [
+//     {
+//       "id": 170849,
+//       "name": "CosySoft",
+//       "nick": "cosysoft",
+//       "member_type": "owner",
+//       "production_calendar": 1
+//     }
+//   ]
+// }
+// https://api.huntflow.ru/v2/accounts/170849/applicants?page=1
+// https://api.huntflow.ru/v2/accounts/170849/applicants POST
