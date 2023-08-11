991da467f4cc6ea33efd6b9bc204ede0cc35ad41
Мизин Сергей Юрьевич (4091265) <mizin@vtb.ru>
2023-08-11 18:54:20 +0400

    added parsing rest vacancies by formid

--- src/utils/parseReferer.ts
+++ src/utils/parseReferer.ts
@@ -1,9 +1,9 @@
 export const parseSource = (formid: string) => {
   switch (formid) {
     case 'form571202343': return 'DevOps'
-    case 'https://cosysoft.org/careers/react-middle': return 'React-разработчик Middle'
-    case 'https://cosysoft.org/careers/sales-manager': return 'Sales-менеджер в IT'
-    case 'https://cosysoft.org/careers/system-analyst': return 'Cистемный аналитик Middle | Senior'
+    case 'form607487616': return 'React-разработчик Middle'
+    case 'form571182068': return 'Java-разработчик Middle | Senior'
+    case 'form571186934': return 'Cистемный аналитик Middle | Senior'
     case 'form610251561': return 'IT-рекрутер'
     default: return 'Не указано'
   }
